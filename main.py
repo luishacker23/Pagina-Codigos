38000eccab0f22c130498b2220296752ecb6acf3
Luis Torres <119002870+luishacker23@users.noreply.github.com>
2025-02-10 23:42:26 -0500

    Update 2 main.py

--- main.py
+++ main.py
@@ -13,9 +13,6 @@ PASSWORD = 'unsn xfdz qaob uglx'
 IMAP_SERVER = 'imap.gmail.com'
 
 
-EMAIL = 'juanpedri45u@gmail.com'
-PASSWORD = 'aive swol azfs jizr'
-IMAP_SERVER = 'imap.gmail.com'
 
 # Expresiones regulares para filtrar los asuntos
 ASUNTOS_PERMITIDOS = [
